022ad370c60a01cd34464c53736a2ab7baeca6f7
Jithinksatheesh <jithinksatheesh@gmail.com>
2020-10-20 18:47:52 +0530

    Updates

---
+++ precache-manifest.3609ca099f156ab6645b76f020d66db6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710c88de85d3f51424eab4867cac8a1d",
+    "url": "/protfolio/index.html"
+  },
+  {
+    "revision": "549385118102c06cea2f",
+    "url": "/protfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f7145558d6b69e99574",
+    "url": "/protfolio/static/js/2.b6d8a2a2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/protfolio/static/js/2.b6d8a2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549385118102c06cea2f",
+    "url": "/protfolio/static/js/main.13dad643.chunk.js"
+  },
+  {
+    "revision": "931e23614a4d46aca15d",
+    "url": "/protfolio/static/js/runtime-main.21289a9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/protfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
